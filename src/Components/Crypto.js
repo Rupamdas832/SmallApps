111ede60bccdaad6d2169393327551bfe74de3a7
Rupam <rupamdas832@gmail.com>
2021-02-05 19:07:25 +0530

    crypto api added

--- src/Components/Crypto.js
+++ src/Components/Crypto.js
@@ -4,23 +4,23 @@ const Crypto = () => {
     const [coins, setCoins] = useState([])
 
     const getCoins = async () => {
-        try {
-            fetch("https://api.coinranking.com/v2/coins", {
-                mode: "no-cors"
-            })
-            .then(response => response.json())
-            .then(result => console.log(result))
-        } catch (error) {
-            console.log(error)
-        }
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const response = await fetch(proxyurl + "https://api.coinranking.com/v2/coins?limit=10",{
+            headers: {
+                'x-access-token': 'coinrankinge1eab9daa6c8dc7882f498ff84049614872be9fea18cb6d2'
+            }
+        })
+        const result = await response.json();
+        console.log(result.data)
+       
     }
-
+    
     useEffect(() => {
         getCoins()
     },[])
     return (
         <div>
-            crypto
+            
         </div>
     )
 }
