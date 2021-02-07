4d7d5f3f06f338de54971910b7cad77d2f47c09b
Rupam <rupamdas832@gmail.com>
2021-02-07 13:43:18 +0530

    crypto no progress

--- src/Components/Crypto.js
+++ src/Components/Crypto.js
@@ -5,14 +5,13 @@ const Crypto = () => {
 
     const getCoins = async () => {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const response = await fetch(proxyurl + "https://api.coinranking.com/v2/coins?limit=10",{
+        const response = await fetch("https://api.coinranking.com/v2/coins?limit=10",{
             headers: {
                 'x-access-token': 'coinrankinge1eab9daa6c8dc7882f498ff84049614872be9fea18cb6d2'
             }
         })
-        const result = await response.json();
-        console.log(result.data)
-       
+        const {data} = await response.json();
+        console.log(data.coins)
     }
     
     useEffect(() => {
