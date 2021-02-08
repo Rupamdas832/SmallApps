2e1e577fae77b10e1350220779a801ff1a3a30ef
Rupam <rupamdas832@gmail.com>
2021-02-08 10:02:27 +0530

    2nd api tried for crypto

--- src/Components/Crypto.js
+++ src/Components/Crypto.js
@@ -1,25 +1,51 @@
 import React, {useEffect, useState} from 'react'
 
 const Crypto = () => {
-    const [coins, setCoins] = useState([])
+   // const [coins, setCoins] = useState([])
 
     const getCoins = async () => {
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const response = await fetch("https://api.coinranking.com/v2/coins?limit=10",{
+       // const url1 = "https://api.coinranking.com/v2/coins?limit=10";
+       // const key1 = 'coinrankinge1eab9daa6c8dc7882f498ff84049614872be9fea18cb6d2';
+        const url2 = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+        const key2 = "deafcbf6-7fb9-45a7-85de-c889a6eaae83";
+        const response = await fetch(url2,{
+            qs: {
+                'start': '1',
+                'limit': '10',
+                'convert': 'USD'
+            },
             headers: {
-                'x-access-token': 'coinrankinge1eab9daa6c8dc7882f498ff84049614872be9fea18cb6d2'
-            }
+                'X-CMC_PRO_API_KEY': key2,
+                "Accept": "application/json"
+            },
+            json: true,
+            gzip: true
         })
-        const {data} = await response.json();
-        console.log(data.coins)
+        const result = await response.json();
+        console.log(response)
+        
+        /*    fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",{
+            qs: {
+                'start': '1',
+                'limit': '10',
+                'convert': 'USD'
+            },
+            headers: {
+                'X-CMC_PRO_API_KEY': "deafcbf6-7fb9-45a7-85de-c889a6eaae83"
+            },
+            json: true,
+            gzip: true
+        }).then(response => response.json()).then(result => console.log(result))
+
+        */
     }
     
     useEffect(() => {
-        getCoins()
+        getCoins();
     },[])
     return (
         <div>
-            
+            <h1>cryptooooo</h1>
         </div>
     )
 }
